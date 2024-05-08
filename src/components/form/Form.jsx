f417f7fcd1af89b88028f4e35e2732c78c7064e7
seohee <hui3363@gmail.com>
2024-05-08 13:25:08 +0900

    Firebase Auth

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,30 +1,74 @@
 import React from 'react'
 import styles from './Form.module.scss'
+import { useForm } from 'react-hook-form'
 
 // eslint-disable-next-line react/prop-types
-const Form = ({ title }) => {
+const Form = ({ title, getDataForm, firebaseError}) => {
+
+	const { register, handleSubmit, formState: {errors}, reset } = useForm({
+		mode: 'onChange'
+	}) //onChange | onSubmit | onBlur
+
+	const onSubmit = ({email, password}) => {
+		getDataForm(email, password);
+		reset();
+	}
+
+	const userEmail = {
+		required: "필수 필드입니다.",
+		pattern: {
+			value: 6,
+			message: "최소 4자입니다.",
+		}
+	}
+
+	const userPassword = {
+		required: "필수 필드입니다.",
+		minLength: {
+			value: 6,
+			message: "최소 4자입니다.",
+		},
+		maxLength: {
+			value: 13,
+			message: "최대 13자입니다.",
+		}
+
+	}
+
 	return (
-		<form className={styles.form}>
+		<form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
 			<div>
 				<input
 					type="email"
 					placeholder="E-mail"
+					{...register("email", userEmail)}
 				/>
-				<div>
-					<span className={styles.form_error}></span>
-				</div>
+				{errors?.email &&
+					<div>
+						<span className={styles.form_error}>
+							{errors.email.message}
+						</span>
+					</div>
+				}
 			</div>
 			<div>
 				<input
 					type="password"
 					placeholder="Password"
+					{...register("password", userPassword)}
 				/>
-				<div>
-					<span className={styles.form_error}></span>
-				</div>
+				{errors?.password &&
+					<div>
+						<span className={styles.form_error}>
+							{errors.password.message}
+						</span>
+					</div>
+				}
 			</div>
 			<button type='submit'>{title}</button>
-			<span className={styles.form_error}></span>
+			{firebaseError && 
+				<span className={styles.form_error}>{firebaseError}</span>
+			}
 		</form>
 	)
 }
