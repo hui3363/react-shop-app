ee1d4994e9b35607ee29240ef86821a31b1bbc41
seohee <hui3363@gmail.com>
2024-07-19 12:56:06 +0900

    product skeleton

--- src/pages/HomePage/card-list/CardList.jsx
+++ src/pages/HomePage/card-list/CardList.jsx
@@ -3,16 +3,19 @@ import { useAppDispatch, useAppSelector } from '../../../hooks/redux'
 import styles from './CardList.module.scss'
 import CardItem from './card-item/CardItem'
 import { fetchProducts } from '../../../store/products/products.slice'
+import CardSkeleton from '../card-skeleton/CardSkeleton'
 
 const CardList = () => {
 
 	const dispatch = useAppDispatch();
-	const { products } = useAppSelector(state => state.productsSlice)
+	const { products, isLoading } = useAppSelector(state => state.productsSlice)
 	const category = useAppSelector(state => state.categoriesSlice)
 
 	useEffect(() => {
 		dispatch(fetchProducts(category?.toLowerCase()));
 	}, [category])
+
+	if(isLoading) return <CardSkeleton />
 
 	return (
 		<ul className={styles.card_list}>
