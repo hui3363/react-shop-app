5ed4b51e70ade27403fcf3375054b8fac4a8bfd7
seohee <hui3363@gmail.com>
2024-08-27 15:09:40 +0900

    장바구니 가격

--- src/pages/CartPage/cart-list/CartList.jsx
+++ src/pages/CartPage/cart-list/CartList.jsx
@@ -4,7 +4,7 @@ import CartItem from './cart-item/CartItem'
 import styles from './CartList.module.scss'
 
 const CartList = () => {
-	const {products} = useAppSelector( state => state.cartSlice)
+	const { products } = useAppSelector( state => state.cartSlice)
 	return (
 		<div className={styles.cart_list}>
 			{products.map((product) => {
--- src/pages/CartPage/checkout/Checkout.jsx
+++ src/pages/CartPage/checkout/Checkout.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../../../hooks/redux'
+import { getTotalPrice } from '../../../store/cart/cart.slice';
+import styles from './Checkout.module.scss';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../../hooks/useAuth';
 
 const Checkout = () => {
+
+	const cart = useAppSelector(state => state.cartSlice);
+	const dispatch = useAppDispatch();
+
+	useEffect(() =>  {
+		dispatch(getTotalPrice());
+	}, [])
+
+	const {isAuth} = useAuth();
+
 	return (
-		<div>Checkout</div>
+		<div className={styles.checkout}>
+			<div>
+				<p>
+					{" "}
+					<span>합계:</span> $ {cart.totalPrice.toFixed(2)}
+				</p>
+				{isAuth ?
+					<button className={styles.checkout_button}>
+						계산하기
+					</button>
+					:
+					<Link className={styles.checkout_button} to="/login">로그인</Link>
+				}
+			</div>
+		</div>
 	)
 }
 
